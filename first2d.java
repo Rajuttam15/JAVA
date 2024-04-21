940e3f406c0f4425e9049468f73bfe514e06594a
Rajuttam15 <144825848+Rajuttam15@users.noreply.github.com>
2024-04-21 21:12:34 +0530

    Add files via upload

--- first2d.java
+++ first2d.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 public class first2d {
+    public static boolean search(int matrix[][],int key){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==key){
+                    System.out.println("element is found("+ i +","+j+")");
+                    return true;
+                }
+            }
+           
+        }
+        return false;
+ }
     public static void main(String args[]){
         int matrix[][]=new int[3][3];
         int n=matrix.length;
@@ -13,10 +25,11 @@ public class first2d {
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-              System.out.println(matrix[i][j] + " ");
+              System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
+        search(matrix,5);
 
     }
 
---
+++ string1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class string1 {
+    public static void main(String[] args) {
+       // String s;//string are immutable//
+       // Scanner sc=new Scanner(System.in);
+       // s=sc.nextLine();//for print whole sentence we use nextLine// 
+        //System.out.println(s);
+        //String full_name="raj uttam";
+        //System.out.println(full_name.length());
+   
+        String s1="raj";
+        String s2="uttam";
+        String s3=s1+ " "+s2;
+        //String s3=s1.concat(s2);
+        System.out.println(s3 + "full name is");
+        }
+} 
